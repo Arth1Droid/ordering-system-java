e34a5df384d905cc8efb5380afda8cf7fbdd7aa7
ArthDroid1 <barbosarthur7@gmail.com>
2026-03-15 19:16:43 -0300

    Start Project

---
+++ src/main/java/br/dev/arthdroid1/SpringCRUDProject/SpringCrudProjectApplication.java
@@ -0,0 +1,13 @@
+package br.dev.arthdroid1.SpringCRUDProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCrudProjectApplication.class, args);
+	}
+
+}
