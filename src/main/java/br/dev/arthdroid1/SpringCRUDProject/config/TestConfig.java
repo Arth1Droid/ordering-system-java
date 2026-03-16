67ccbcf50245e144f92bef09d488cbdd38df5e5f
ArthDroid1 <barbosarthur7@gmail.com>
2026-03-16 11:39:27 -0300

    Update TestConfig with Order class test's

--- src/main/java/br/dev/arthdroid1/SpringCRUDProject/config/TestConfig.java
+++ src/main/java/br/dev/arthdroid1/SpringCRUDProject/config/TestConfig.java
@@ -1,12 +1,15 @@
 package br.dev.arthdroid1.SpringCRUDProject.config;
 
+import br.dev.arthdroid1.SpringCRUDProject.models.entities.Order;
 import br.dev.arthdroid1.SpringCRUDProject.models.entities.User;
+import br.dev.arthdroid1.SpringCRUDProject.repositories.OrderRepository;
 import br.dev.arthdroid1.SpringCRUDProject.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 @Configuration
@@ -14,6 +17,8 @@ import java.util.Arrays;
 public class TestConfig implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private OrderRepository orderRepository;
 
 
     @Override
@@ -21,6 +26,11 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Artwa Broa", "artwa@gmail.com", "9888388888", "123456");
         User u2= new User(null, "Linda Brim", "linda@gmail.com", "9888379688", "123457");
 
+        Order order1 = new Order(null, Instant.parse("2025-06-20T19:53:07Z"), u1);
+        Order order2 = new Order(null, Instant.parse("2025-07-20T18:53:06Z"), u2);
+        Order order3 = new Order(null, Instant.parse("2025-07-20T16:30:00Z"), u1);
+
         userRepository.saveAll(Arrays.asList(u1,u2));
+        orderRepository.saveAll(Arrays.asList(order1,order2,order2));
     }
 }
