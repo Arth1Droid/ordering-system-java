7155297121b7bba14b9927038b2a5c3ae8bc91d4
ArthDroid1 <barbosarthur7@gmail.com>
2026-03-16 10:51:12 -0300

    Create class UserController in controllers

--- src/main/java/br/dev/arthdroid1/SpringCRUDProject/repositories/UserRepository.java
+++ src/main/java/br/dev/arthdroid1/SpringCRUDProject/repositories/UserRepository.java
@@ -2,7 +2,9 @@ package br.dev.arthdroid1.SpringCRUDProject.repositories;
 
 import br.dev.arthdroid1.SpringCRUDProject.models.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 
 
