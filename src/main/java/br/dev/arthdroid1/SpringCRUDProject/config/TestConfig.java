3ab86092484a532f98a4208d76d9df6b422a4801
ArthDroid1 <barbosarthur7@gmail.com>
2026-03-17 21:35:51 -0300

    Add tests with Category in TestConfig

--- src/main/java/br/dev/arthdroid1/SpringCRUDProject/config/TestConfig.java
+++ src/main/java/br/dev/arthdroid1/SpringCRUDProject/config/TestConfig.java
@@ -1,17 +1,20 @@
 package br.dev.arthdroid1.SpringCRUDProject.config;
 
-import br.dev.arthdroid1.SpringCRUDProject.models.entities.Order;
-import br.dev.arthdroid1.SpringCRUDProject.models.entities.User;
-import br.dev.arthdroid1.SpringCRUDProject.models.entities.enums.OrderStatus;
-import br.dev.arthdroid1.SpringCRUDProject.repositories.OrderRepository;
-import br.dev.arthdroid1.SpringCRUDProject.repositories.UserRepository;
+import java.time.Instant;
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-import java.time.Instant;
-import java.util.Arrays;
+import br.dev.arthdroid1.SpringCRUDProject.models.entities.Category;
+import br.dev.arthdroid1.SpringCRUDProject.models.entities.Order;
+import br.dev.arthdroid1.SpringCRUDProject.models.entities.User;
+import br.dev.arthdroid1.SpringCRUDProject.models.entities.enums.OrderStatus;
+import br.dev.arthdroid1.SpringCRUDProject.repositories.CategoryRepository;
+import br.dev.arthdroid1.SpringCRUDProject.repositories.OrderRepository;
+import br.dev.arthdroid1.SpringCRUDProject.repositories.UserRepository;
 
 @Configuration
 @Profile("test")
@@ -20,18 +23,25 @@ public class TestConfig implements CommandLineRunner {
     private UserRepository userRepository;
     @Autowired
     private OrderRepository orderRepository;
-
-
+    @Autowired
+    private CategoryRepository categoryRepository;
+ 
     @Override
     public void run(String... args) throws Exception {
+    	
+        Category category1 = new Category("Eletronics", null);
+        Category category2 = new Category("Books", null);
+        Category category3 = new Category("Computers", null);
+        
         User u1 = new User(null, "Artwa Broa", "artwa@gmail.com", "9888388888", "123456");
         User u2= new User(null, "Linda Brim", "linda@gmail.com", "9888379688", "123457");
 
         Order order1 = new Order(null, Instant.parse("2025-06-20T19:53:07Z"), OrderStatus.PAID, u1);
         Order order2 = new Order(null, Instant.parse("2025-07-20T18:53:06Z"), OrderStatus.WAITING_PAYMENT, u2);
         Order order3 = new Order(null, Instant.parse("2025-07-20T16:30:00Z"), OrderStatus.WAITING_PAYMENT, u1);
-
+        
         userRepository.saveAll(Arrays.asList(u1,u2));
         orderRepository.saveAll(Arrays.asList(order1,order2,order2));
+        categoryRepository.saveAll(Arrays.asList(category1, category2, category3));
     }
 }
