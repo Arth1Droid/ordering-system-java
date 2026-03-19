fea77edc6275f86d4c3576aa0049bef1347d1cde
ArthDroid1 <barbosarthur7@gmail.com>
2026-03-19 19:32:28 -0300

    Add exception handling in class's

--- src/main/java/br/dev/arthdroid1/SpringCRUDProject/controllers/UserController.java
+++ src/main/java/br/dev/arthdroid1/SpringCRUDProject/controllers/UserController.java
@@ -1,15 +1,22 @@
 package br.dev.arthdroid1.SpringCRUDProject.controllers;
 
-import br.dev.arthdroid1.SpringCRUDProject.models.entities.User;
-import br.dev.arthdroid1.SpringCRUDProject.services.UserService;
+import java.net.URI;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.util.List;
+import br.dev.arthdroid1.SpringCRUDProject.models.entities.User;
+import br.dev.arthdroid1.SpringCRUDProject.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -28,5 +35,24 @@ public class UserController {
         User userFound = userService.findById(id);
         return  ResponseEntity.ok().body(userFound);
 
+    }
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User user){
+    	user = userService.insert(user);
+    	URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+    	return  ResponseEntity.created(uri).body(user);
+    }
+    
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+    	userService.delete(id);
+    	return ResponseEntity.noContent().build();
+    }
+    
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user){
+    	user = userService.update(id, user);
+    	return ResponseEntity.ok().body(user);
+    	
     }
 }
--- src/main/java/br/dev/arthdroid1/SpringCRUDProject/services/UserService.java
+++ src/main/java/br/dev/arthdroid1/SpringCRUDProject/services/UserService.java
@@ -1,26 +1,65 @@
 package br.dev.arthdroid1.SpringCRUDProject.services;
 
-import br.dev.arthdroid1.SpringCRUDProject.models.entities.User;
-import br.dev.arthdroid1.SpringCRUDProject.repositories.UserRepository;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
-
-import java.util.List;
-import java.util.Optional;
+import br.dev.arthdroid1.SpringCRUDProject.models.entities.User;
+import br.dev.arthdroid1.SpringCRUDProject.repositories.UserRepository;
+import br.dev.arthdroid1.SpringCRUDProject.services.exceptions.DatabaseException;
+import br.dev.arthdroid1.SpringCRUDProject.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository repository;
+    
+    public User insert(User user) {
+    	return repository.save(user);
+    }
+    
+    public void delete(Long id) {
+    	try {
+    		repository.deleteById(id);
+    	}
+    	catch(EmptyResultDataAccessException ex) {
+    		throw new ResourceNotFoundException(id);
+    	}
+    	catch(DataIntegrityViolationException ex) {
+    		throw new DatabaseException(ex.getMessage());
+    	}
+    		
+    }
+    
+    public User update(Long id, User user) {
+    	try {
+	    	User entity = repository.getReferenceById(id);
+	    	updateDate(entity,user);
+	    	return repository.save(entity);
+    	}
+    	catch(EntityNotFoundException ex) {
+    		throw new ResourceNotFoundException(id);
+    	}
+    }
 
-    public List<User> findAll(){
+    private void updateDate(User entity, User user) {
+		entity.setName(user.getName());
+		entity.setEmail(user.getEmail());
+		entity.setPhone(user.getPhone());
+		
+	}
+
+	public List<User> findAll(){
         return repository.findAll();
     }
 
     public User findById(Long id) {
         return repository.findById(id).orElseThrow(()
-                -> new RuntimeException("User with id: " + id + " not founded "));
+                -> new ResourceNotFoundException( id));
     }
 }
