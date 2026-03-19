4fba2c43aa3c1af0116e3d9d1fa225a14297e993
ArthDroid1 <barbosarthur7@gmail.com>
2026-03-19 12:09:40 -0300

    Add one-to-one association Order -> Payment

--- src/main/java/br/dev/arthdroid1/SpringCRUDProject/models/entities/Order.java
+++ src/main/java/br/dev/arthdroid1/SpringCRUDProject/models/entities/Order.java
@@ -9,14 +9,7 @@ import java.util.Set;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import br.dev.arthdroid1.SpringCRUDProject.models.entities.enums.OrderStatus;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "tb_order")
@@ -39,6 +32,9 @@ public class Order implements Serializable {
 	
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
+
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
 
 	public Order() {
 
@@ -75,7 +71,15 @@ public class Order implements Serializable {
 		this.client = client;
 	}
 
-	
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
+
 	public OrderStatus getOrderStatus() {
 		return OrderStatus.valueOf(orderStatus);
 	}
